8db26682f81716effec2902c1818947a258a8c41
actionbot <actionbot@github.com>
2024-10-06 07:48:32 +0000

    Deploy website - based on e43b620da6e7b47028cfb4b4a97d4418bde1818e

---
+++ assets/js/814f3328.8eb1cb69.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkwebsite=self.webpackChunkwebsite||[]).push([[7472],{5513:e=>{e.exports=JSON.parse('{"title":"Recent posts","items":[{"title":"index","permalink":"/colornames-i18n/blog/index","unlisted":false,"date":"2024-10-06T07:45:21.000Z"}]}')}}]);
